cb648cc3387ad5f460b276697dda3bad0386aaf3
jaehyukj3 <87752210+jaehyukj3@users.noreply.github.com>
2025-07-13 11:49:45 +0000

    chore:  Sass의 @import "tailwindcss"; 경고 대응을 위해 global.scss → global.css 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
+import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Sidebar from "./components/Sidebars/Sidebar";
 
